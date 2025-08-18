ef140b1510a5eb6a7554f51ebf3da86d79aa9c5b
Daniel Soriano <danielsoriano559@gmail.com>
2025-08-18 08:27:18 -0700

    add payrollProgramWithExcptHndl3.2.py

---
+++ payrollProgramWithExcptHndl3.2.py
@@ -0,0 +1,18 @@
+# Daniel Soriano - Payroll Program with Exception Handling code along
+
+sh = input("Enter Hours: ")
+sr = input("Enter Rate: ")
+try:
+    fh = float(sh)
+    fr = float(sr)
+except:
+    print("Error, please enter numeric input")
+    quit()
+print(fh, fr)
+if fh > 40:
+    reg = fr * fh
+    otp = (fh-40.0) * (fr + (fr * .5)) # fixed code from video
+    xp = reg + otp
+else:
+    xp = fh * fr
+print("Pay:", xp)
